8b18bfb6e1e9066d07da81cc6d57aed488333d7b
Muhammad Zourdy <zhafarmuhammad12@gmail.com>
2022-09-10 17:34:27 +0200

    Simple redux utilities

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,16 @@ import "@styles/styles.scss";
 import "normalize.css/normalize.css";
 
 import { App } from "@components/app";
+import { store } from "@features/app/store";
 import React from "react";
 import ReactDom from "react-dom";
+import { Provider } from "react-redux";
 
-ReactDom.render(<App />, document.getElementById("root"));
+ReactDom.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>,
+  document.getElementById("root")
+);
