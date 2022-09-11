fc5e07d2f9e5bfc16ec7dd39d984b6f73185ad83
Muhammad Zourdy <zhafarmuhammad12@gmail.com>
2022-09-11 11:30:57 +0200

    Create interface models for data fetched from the api

---
+++ src/models/index.ts
@@ -0,0 +1,12 @@
+import {
+  CoinMarketChart as CoinMarketChartInterface,
+  CoinMarketChartList as CoinMarketChartListInterface,
+} from "@src/models/api/coin-market-chart.model";
+import { GenericState as GenericStateInterface } from "@src/models/common/generic-state";
+
+import { Coin as CoinInterface } from "./api/coin.model";
+
+export type GenericState<T> = GenericStateInterface<T>;
+export type Coin = CoinInterface;
+export type CoinMarketChart = CoinMarketChartInterface;
+export type CoinMarketChartList = CoinMarketChartListInterface;
