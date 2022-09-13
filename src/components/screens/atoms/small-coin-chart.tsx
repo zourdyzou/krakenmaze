2794fd3534ae88afc3117261466b95f51aa6cde2
Muhammad Zourdy <zhafarmuhammad12@gmail.com>
2022-09-13 11:50:56 +0200

    Create `small-coin-chart` component

---
+++ src/components/screens/atoms/small-coin-chart.tsx
@@ -0,0 +1,63 @@
+import { useAppSelector } from "@features/app/hooks";
+import { selectCoinMarketChartList } from "@features/coins/marketChartList-slice";
+import { useTheme } from "@material-ui/core";
+import { Skeleton } from "@material-ui/lab";
+import { Coin, CoinMarketChart } from "@src/models";
+import React from "react";
+import { Area, AreaChart, YAxis } from "recharts";
+
+interface Props {
+  coin: Coin;
+  dataKey: keyof CoinMarketChart;
+}
+
+export const SmallCoinChart: React.FunctionComponent<Props> = ({ coin, dataKey }) => {
+  const theme = useTheme();
+  const coinMarketChartList = useAppSelector(selectCoinMarketChartList);
+  const gain = coin.priceChangePercentage24H > 0;
+
+  const formatRawData = () => {
+    const formattedData = [];
+
+    for (const array of coinMarketChartList.value[coin.id][dataKey]) {
+      formattedData.push({ date: array[0], datakey: array[1] });
+    }
+
+    return formattedData;
+  };
+
+  return (
+    <>
+      {!coinMarketChartList.value[coin.id] ? (
+        <Skeleton animation="wave" height={60} width={100} id="titleSkeleton" />
+      ) : (
+        <AreaChart width={100} height={60} data={formatRawData()} margin={{ top: 0, right: 8, left: 8, bottom: 0 }}>
+          <defs>
+            <linearGradient id={gain ? "gain" : "loss"} x1="0" y1="0" x2="0" y2="1">
+              <stop
+                offset="5%"
+                stopColor={gain ? theme.palette.success.main : theme.palette.error.main}
+                stopOpacity={0.5}
+              />
+              <stop
+                offset="60%"
+                stopColor={gain ? theme.palette.success.main : theme.palette.error.main}
+                stopOpacity={0}
+              />
+            </linearGradient>
+          </defs>
+          <YAxis domain={[(dataMin: number) => dataMin * 0.95, (dataMax: number) => dataMax * 1.05]} hide />
+          <Area
+            type="monotone"
+            dataKey="dataKey"
+            dot={false}
+            animationDuration={3000}
+            stroke={gain ? theme.palette.success.main : theme.palette.error.main}
+            fillOpacity={1}
+            fill={`url(#${gain ? "gain" : "loss"})`}
+          />
+        </AreaChart>
+      )}
+    </>
+  );
+};
