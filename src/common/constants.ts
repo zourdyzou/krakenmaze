054eb5f774660b8d5bb435639dfc6a69df817441
Muhammad Zourdy <zhafarmuhammad12@gmail.com>
2022-09-19 03:07:00 +0200

    Update endpoints API and API config headers

--- src/common/constants.ts
+++ src/common/constants.ts
@@ -1,15 +1,28 @@
 import { AxiosRequestConfig } from "axios";
 
-export const API_CONFIG: AxiosRequestConfig = {
-  baseURL: "https://api.coingecko.com/api/v3",
-  responseType: "json",
-  method: "GET",
-  headers: {
-    "X-XSS-Protection": "1; mode=block",
-    "X-Frame-Options": "DENY",
-    "X-Content-Type-Options": "nosniff",
-    "Strict-Transport-Security": "max-age=63072000; includeSubDomains; preload",
-  },
+type ApiConfigFunction = (server: "coinGecko" | "etherscan") => AxiosRequestConfig;
+
+export const API_CONFIG: ApiConfigFunction = (server: "coinGecko" | "etherscan") => {
+  switch (server) {
+    case "coinGecko":
+      return {
+        baseURL: "https://api.coingecko.com/api/v3",
+        responseType: "json",
+        method: "GET",
+        headers: {
+          "X-XSS-Protection": "1; mode=block",
+          "X-Frame-Options": "DENY",
+          "X-Content-Type-Options": "nosniff",
+          "Strict-Transport-Security": "max-age=63072000; includeSubDomains; preload",
+        },
+      };
+    case "etherscan":
+      return {
+        baseURL: "https://api.etherscan.io",
+        responseType: "json",
+        method: "GET",
+      };
+  }
 };
 
 export const drawerWidth = 260;
