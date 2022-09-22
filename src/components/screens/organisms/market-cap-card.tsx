5f412c790c6bb036fa863a95ac2492a152cf5701
Muhammad Zourdy <zhafarmuhammad12@gmail.com>
2022-09-22 09:45:41 +0200

    Finished making the market cap donut chart

---
+++ src/components/screens/organisms/market-cap-card.tsx
@@ -0,0 +1,44 @@
+import React from "react";
+import { CardHeader } from "@material-ui/core";
+import { makeStyles, Theme } from "@material-ui/core/styles";
+import { Skeleton } from "@material-ui/lab";
+
+import { shortenNumber } from "@/common/helpers/shorten-number";
+import { GlobalCoinDataDonutChart as MarketCapDonutChart } from "@/components/screens/atoms/market-cap-donut-chart";
+import { CardLayout } from "@/components/screens/molecules/card-layout";
+import { selectGlobalCoinData } from "@/features/global-coin-data-slice";
+import { useAppSelector } from "@/hooks/*";
+
+const useStyles = makeStyles((theme: Theme) => ({
+  chartWrapper: {
+    height: "100%",
+    width: "100%",
+    marginTop: -35,
+  },
+}));
+
+export const MarketCapCard: React.FunctionComponent = () => {
+  const classes = useStyles(0);
+
+  const globalCoinData = useAppSelector(selectGlobalCoinData);
+
+  return (
+    <CardLayout>
+      <CardHeader
+        title="Market Cap"
+        titleTypographyProps={{ variant: "body2", color: "textSecondary" }}
+        subheader={
+          globalCoinData.value !== null ? (
+            `US$${shortenNumber(globalCoinData.value.totalMarketCap.usd)}`
+          ) : (
+            <Skeleton animation="wave" height={32} width={50} />
+          )
+        }
+        subheaderTypographyProps={{ variant: "h6", color: "textPrimary" }}
+      />
+      <div className={classes.chartWrapper}>
+        <MarketCapDonutChart />
+      </div>
+    </CardLayout>
+  );
+};
--- src/pages/analytics/coins.tsx
+++ src/pages/analytics/coins.tsx
@@ -1,15 +1,14 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { Grid } from "@material-ui/core";
 import { makeStyles, Theme, useTheme } from "@material-ui/core/styles";
 
 import { BannerCardSmall } from "@/components/screens/molecules/banner-card-small";
 import { CoinDominanceCard } from "@/components/screens/organisms/coin-dominance-card";
 import { GasOracleCard } from "@/components/screens/organisms/gas-indicator-card";
-import { GlobalCoinDataCard } from "@/components/screens/organisms/global-coin-data-card";
+import { MarketCapCard } from "@/components/screens/organisms/market-cap-card";
 import { TopCoinsCard } from "@/components/screens/organisms/top-coins-card";
 import { TrendingCoinsCard } from "@/components/screens/organisms/trending-coins-card";
-import { fetchGlobalCoinData, selectGlobalCoinData } from "@/features/global-coin-data-slice";
-import { useAppDispatch, useAppSelector } from "@/hooks/*";
+import { VolumeCard } from "@/components/screens/organisms/volume-card";
 
 const useStyles = makeStyles((theme: Theme) => ({
   rootWrapper: {
@@ -29,16 +28,6 @@ const useStyles = makeStyles((theme: Theme) => ({
 export const CoinsPage: React.FunctionComponent = () => {
   const classes = useStyles();
   const theme = useTheme();
-  const dispatch = useAppDispatch();
-
-  const globalCoinData = useAppSelector(selectGlobalCoinData);
-
-  // fetch globalCoinData here as used in two cards (prevent fetching twice if dispatch in card)
-  useEffect(() => {
-    if (globalCoinData.value === null && globalCoinData.status === "IDLE") {
-      dispatch(fetchGlobalCoinData());
-    }
-  }, [dispatch, globalCoinData.value, globalCoinData.status]);
 
   return (
     <Grid className={classes.rootWrapper} spacing={3} direction="row" justify="center" alignItems="stretch" container>
@@ -66,10 +55,10 @@ export const CoinsPage: React.FunctionComponent = () => {
             <BannerCardSmall />
           </Grid>
           <Grid item xs={12} style={{ height: `calc(50% - ${42.5 + theme.spacing(3)}px)` }}>
-            <GlobalCoinDataCard type="marketcap" />
+            <MarketCapCard />
           </Grid>
           <Grid item xs={12} style={{ height: `calc(50% - ${42.5 + theme.spacing(3)}px)` }}>
-            <GlobalCoinDataCard type="volume" />
+            <VolumeCard />
           </Grid>
         </Grid>
       </Grid>
