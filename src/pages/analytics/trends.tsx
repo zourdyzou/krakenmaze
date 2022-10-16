e372868ddd83dc9c3a0c0657e000fa2587e6a3ce
Muhammad Zourdy <zhafarmuhammad12@gmail.com>
2022-10-16 11:01:59 +0200

    Implementing new features `fear-greed-index` components to trends page

--- src/pages/analytics/trends.tsx
+++ src/pages/analytics/trends.tsx
@@ -1,9 +1,10 @@
 import React from "react";
 import { Grid, Hidden } from "@material-ui/core";
-import { makeStyles, Theme } from "@material-ui/core/styles";
+import { makeStyles, Theme, useTheme } from "@material-ui/core/styles";
 
 import { CoinCorrelationCard } from "@/components/screens/organisms/coin-correlation-card";
-import { InstitutionHoldersCard } from "@/components/screens/organisms/institution-holders-card";
+import { FearGreedIndexCard } from "@/components/screens/organisms/fear-greed-index-card";
+import { MarketSentimentCard } from "@/components/screens/organisms/market-sentiment-card";
 
 const useStyles = makeStyles((theme: Theme) => ({
   wrapper: {
@@ -12,20 +13,34 @@ const useStyles = makeStyles((theme: Theme) => ({
       height: "100%",
     },
   },
+  innerWrapper: {
+    height: "100%",
+    "& > .MuiGrid-item:not(:last-child)": {
+      marginBottom: theme.spacing(3),
+    },
+  },
 }));
 
 export const TrendsPage: React.FunctionComponent = () => {
   const classes = useStyles();
+  const theme = useTheme();
 
   return (
     <Grid container className={classes.wrapper} spacing={3} direction="row" justify="center" alignItems="stretch">
-      <Grid item xs={12} xl={9}>
+      <Grid item xs={12} lg={8} xl={9}>
         <CoinCorrelationCard />
       </Grid>
 
-      <Hidden lgDown>
-        <Grid item xl={3}>
-          <InstitutionHoldersCard />
+      <Hidden mdDown>
+        <Grid item lg={4} xl={3}>
+          <Grid container className={classes.innerWrapper} spacing={0}>
+            <Grid item xs={12} style={{ height: `calc(50% - ${theme.spacing(3) / 2}px)` }}>
+              <FearGreedIndexCard />
+            </Grid>
+            <Grid item xs={12} style={{ height: `calc(50% - ${theme.spacing(3) / 2}px)` }}>
+              <MarketSentimentCard />
+            </Grid>
+          </Grid>
         </Grid>
       </Hidden>
     </Grid>
--- src/stories/Page.stories.tsx
+++ src/stories/Page.stories.tsx
@@ -1,6 +1,7 @@
 import React from "react";
-import { ComponentStory, ComponentMeta } from "@storybook/react";
-import { within, userEvent } from "@storybook/testing-library";
+import { ComponentMeta, ComponentStory } from "@storybook/react";
+import { userEvent, within } from "@storybook/testing-library";
+
 import { Page } from "./Page";
 
 export default {
@@ -19,6 +20,7 @@ export const LoggedOut = Template.bind({});
 export const LoggedIn = Template.bind({});
 
 // More on interaction testing: https://storybook.js.org/docs/react/writing-tests/interaction-testing
+// @ts-ignore
 LoggedIn.play = async ({ canvasElement }) => {
   const canvas = within(canvasElement);
   const loginButton = await canvas.getByRole("button", { name: /Log in/i });
